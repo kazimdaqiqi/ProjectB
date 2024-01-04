e3c40ce4c81ad001c3da5b6dc428915fc2002578
Kazim Daqiqi <Kazim Daqiqi@Mohamma.mynetworksettings.com>
2024-01-03 21:48:27 -0500

    Implemented Add()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -1,10 +1,27 @@
 package b;
 
 public class DemoB {
-	public static void main (String args[]) {
-		
+	public static void main(String args[]) {
+
 		System.out.println("Kazim");
 		System.out.println("Batch 11 Git Lecture...");
+	}
+
+	// Sprint 2 automation
+	public static void add(int a, int b) {
+
+		System.out.println(a + b);
+
+	}
+
+	// Sprint 2 automation
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
+	}
+
+	// Sprint 2 automation
+	public static void divide(int a, int b) {
+		System.out.println(a / b);
 	}
 
 }
