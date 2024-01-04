297b7e84025a8afe255db59d8c944d13f749cc9c
kazimdaqiqi <155591206+kazimdaqiqi@users.noreply.github.com>
2024-01-03 21:49:51 -0500

    Update DemoB.java

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -9,7 +9,6 @@ public class DemoB {
 
 	// Sprint 2 automation
 	public static void add(int a, int b) {
-
 		System.out.println(a + b);
 
 	}
@@ -23,5 +22,10 @@ public class DemoB {
 	public static void divide(int a, int b) {
 		System.out.println(a / b);
 	}
+	// Sprint 2 automation
+	public static void divide(int a, int b, int c) {
+		System.out.println(a / b);
+	}
+
 
 }
